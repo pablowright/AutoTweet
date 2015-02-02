da49810210ec60b3b28f4cb5229b8982fe698529
pablowright <wrightp@Pauls-MacBook-Pro.local>
2015-02-02 13:40:18 -0500

    Set character set to utf8
    Set character set to utf8 to fix "?" showing up in Twitter feed on some
    browsers.

--- AutoTweet.php
+++ AutoTweet.php
@@ -1,13 +1,15 @@
 <?php
 
-// AutoTweet.php (ver 1.02, 12-30-2014). Script to auto-post tweets from a database. Supports multiple users. Run from cron. 
-// by Paul R. Wright PRW. pablo.wright@gmail.com. Please email or tweet me if you find this useful or have 
+// AutoTweet.php (ver 1.02, 10-30-2013). Script to auto-post tweets from a database. Supports multiple users. Run from cron. 
+// by Paul R. Wright PRW. pablo.wright@gmail.com Please email or tweet me if you find this useful or have 
 // suggestions/modifications.
-// Requires MySQL, php and the twitteroauth library by Abraham Williams. https://github.com/abraham/twitteroauth and of course,
-// Twitter app.
+// Requires MySQL, php and the twitteroauth library by Abraham Williams. https://github.com/abraham/twitteroauth
+
+// Get rid of "?" in some Web browsers. maybe?:
+header('Content-Type: text/html; charset=utf-8');
 
 // Use twitteroauth library:
-require_once('EDIT PATH/TO/twitteroauth.php');
+require_once('../twitteroauth/twitteroauth/twitteroauth.php');
 
 // Set error reporting level. "E_ALL" is good for debugging.
 error_reporting(E_ALL);
@@ -27,8 +29,8 @@ if ( $id != 0 ){
 $userID = $id;
 }
 else {
-error_log('User does not exist '.$id. "\n", 3, "AutoTweet.log"); 
-  exit("No user by this id. Let's go listen to Science Friday instead.");
+error_log('User does not exist '.$id. "\n", 3, "tweeterErrors.log"); 
+  exit("No user by this id. Let's go listen to Science Friday.");
 }
 
 // If called from command line:
@@ -38,13 +40,32 @@ error_log('User does not exist '.$id. "\n", 3, "AutoTweet.log");
 // Connect to DB; Execute Query:
 include 'db-conn.php';
 $link = mysqli_connect("$server","$user","$pass","$database") or die("Error " . mysqli_error($link));
+
+// ============================== Set character set to utf8 ==================================
+// $link = mysqli_connect('localhost', 'my_user', 'my_password', 'test');
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+/* change character set to utf8 */
+
+if (!mysqli_set_charset($link, "utf8")) {
+    printf("Error loading character set utf8: %s\n", mysqli_error($link));
+} else {
+    printf("Current character set: %s\n", mysqli_character_set_name($link));
+}
+
+// ==========================================================================================================
 $query = "SELECT id, posts, posted FROM tweets where userID='$userID' and posted = 0 ORDER by RAND() LIMIT 0,1" or die("Error in the consult.." . mysqli_error($link));
 $result = mysqli_query($link, $query);
 
 //Check results for enpty set:
 if(mysqli_num_rows($result)==0){
   $emptySetError = "{$userID}  {$tweetContentDate}";
-  error_log('No tweets found for user '.$emptySetError. "\n", 3, "AutoTweet.log"); 
+  error_log('No tweets found for user '.$emptySetError. "\n", 3, "tweeterErrors.log"); 
   exit("No tweets for this user.");
 }
 else {  
@@ -83,11 +104,12 @@ $statusSuccess = "{$tweetContentDate}  TwitterUser {$userID} tweeted {$tweetStr}
 $statusError = "{$statusCode}  {$tweetContentDate} TwitterUser {$userID}";
 
 if ($connection->http_code == 200) {
- 		error_log('Success ' .$statusSuccess."\n", 3, "AutoTweet.log");
+ 		error_log('Success ' .$statusSuccess."\n", 3, "tweeterErrors.log");
 		} else {
-		error_log('Error posting to twitter: '.$statusError."\n", 3, "AutoTweet.log");
+		error_log('Error posting to twitter: '.$statusError."\n", 3, "tweeterErrors.log");
 	}
-		
+
+echo "<br />";		
 echo "$tweetStr";		
 
 ?>
